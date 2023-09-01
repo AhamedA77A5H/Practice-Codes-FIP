8fcf960ca62e7a7d4dc38b53f8b7888e0f7aaf88
AhamedA77A5H <aayyash1234@icloud.com>
2023-09-01 09:04:49 +0530

    managers

--- Session_00/amber_bakery/bakery_system.cpp
+++ Session_00/amber_bakery/bakery_system.cpp
@@ -14,6 +14,8 @@ struct Items
     int stock;
 };
 
+void manager_login();
+
 int main()
 {
     int user_type;
@@ -38,6 +40,7 @@ int main()
             break;
         case 3:
             cout << "----------- Welcome Manager -----------\n";
+            manager_login();
             break;
         default:
             cout << "Invalid user type selected. Please select a valid user type (1, 2, or 3).\n";
@@ -47,3 +50,72 @@ int main()
     return 0;
 }
 
+void manager_login()
+{
+    User user;
+    string inputUsername, inputPassword;
+    bool loggedIn = false;
+
+    cout << "Enter username: ";
+    cin >> inputUsername;
+    cout << "Enter password: ";
+    cin >> inputPassword;
+
+    ifstream file("managers.txt");
+    if (file.is_open())
+    {
+        while (file >> user.username >> user.password)
+        {
+            if (inputUsername == user.username && inputPassword == user.password)
+            {
+                loggedIn = true;
+                break;
+            }
+        }
+        file.close();
+    }
+
+    if (loggedIn)
+    {
+        cout << "Login successful!" << endl;
+        int choice;
+        do
+        {
+            cout << "Menu:" << endl;
+            cout << "1. Add Item" << endl;
+            cout << "2. Remove Item" << endl;
+            cout << "3. Edit Item" << endl;
+            cout << "4. Add New Manager" << endl;
+            cout << "0. Exit" << endl;
+            cout << "Enter your choice: ";
+            cin >> choice;
+
+            switch (choice)
+            {
+                case 1:
+                    // Add item 
+                    break;
+                case 2:
+                    // Remove item 
+                    break;
+                case 3:
+                    // Edit item 
+                    break;
+                case 4:
+                    // Add new manager
+                    break;
+                case 0:
+                    cout << "Exiting..." << endl;
+                    break;
+                default:
+                    cout << "Invalid choice!" << endl;
+                    break;
+            }
+        } while (choice != 0);
+    }
+    else
+    {
+        cout << "Login failed!" << endl;
+    }
+
+}
